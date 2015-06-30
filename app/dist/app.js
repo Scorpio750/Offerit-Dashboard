d1f97fa95ccc2737075e64bd107aedb9fb4c7407
Patrick Wu <scorpio7500@gmail.com>
2015-06-30 17:50:51 -0400

    offer panel switches between top and new offers

--- app/dist/app.js
+++ app/dist/app.js
@@ -17400,21 +17400,30 @@ $(document).ready(function() {
 		});
 	});
 $(document).ready(function() {
-    $('.menu-btn').click(function() {
-        var notThisOne = $(this);
-
-        $('.menu-btn').each(function() {
+	$('.menu-btn').click(function() {
+		var notThisOne = $(this);
+		$('.menu-btn').each(function() {
             // ensures only one dropdown is active at any given time
             if ($(this).attr('id') !== notThisOne.attr('id')) {
-                $(this).next('.menu').slideUp({ easing: "swing" });
+            	$(this).next('.menu').slideUp({ 
+            		duration: 300,
+            		easing: "swing" 
+            	});
             } else {
-                $(event.currentTarget).next('.menu').slideToggle({ easing: "swing" });
+            	$(event.currentTarget).next('.menu').slideToggle({ 
+            		duration: 300,
+            		easing: "swing" 
+            	});
             }
         });
-
-    });
-
-    $('#h_chart-btn').hover(function() {
-    	$(this).find('fa-stack-1x').removeClass('fa-bars').addClass('fa-bar-chart');
-    });
+	});
+	$('.optn-btn').click(function() {
+		$(this).css({
+			transform: 'rotateY(180deg)'
+		});
+		$('#top-o').toggleClass('hidden');	
+		$('#new-o').toggleClass('hidden');	
+		$('#top-offer-list').toggleClass('hidden');
+		$('#new-offer-list').toggleClass('hidden');
+	});
 });
