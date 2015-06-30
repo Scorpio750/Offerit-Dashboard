d1713b6657404804ed0cdf3c49096da58e284ee3
Patrick Wu <scorpio7500@gmail.com>
2015-06-30 13:19:35 -0400

    added hover color change for panels

--- app/dist/app.js
+++ app/dist/app.js
@@ -17406,11 +17406,15 @@ $(document).ready(function() {
         $('.menu-btn').each(function() {
             // ensures only one dropdown is active at any given time
             if ($(this).attr('id') !== notThisOne.attr('id')) {
-                $(this).next('.menu').slideUp();
+                $(this).next('.menu').slideUp({ easing: "swing" });
             } else {
-                $(event.currentTarget).next('.menu').slideToggle();
+                $(event.currentTarget).next('.menu').slideToggle({ easing: "swing" });
             }
         });
 
+    });
+
+    $('#h_chart-btn').hover(function() {
+    	$(this).find('fa-stack-1x').removeClass('fa-bars').addClass('fa-bar-chart');
     });
 });
